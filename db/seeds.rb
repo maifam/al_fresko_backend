7534c2e362533a282d26a3afcedc0f47c4bf4ff3
Mai Pham <maitphm@gmail.com>
2021-02-23 21:27:58 -0500

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,14 @@ Review.create(
     restaurant_id: 1
 )
 
+Review.create(
+    content: 'Great place for outdoor dining',
+    rating: 4,
+    likes: 1,
+    user_id: 1,
+    restaurant_id: 3
+)
+
 # Review.create(
 #     content: 'This restaurant made me feel safe while dining out. Cool patio, good service',
 #     rating: 4,
@@ -141,6 +149,167 @@ Restaurant.create(
     od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/lQK_hAFM6PGKZD1N4-74ow/o.jpg',
     fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/3Zi9yt29d8m_MFWFH37kkw/o.jpg',
 )
+#---- 5
+
+Restaurant.create(
+    name: 'Thai Villa',
+    cuisine: 'Thai, Asian Fusion',
+    address: '5 E 19th St G Floor New York, NY 10003',
+    website: 'thaivillanyc.com',
+    phone: '212-802-9999',
+    menu: 'https://direct.chownow.com/order/12278/locations/17123',
+    hours: 'Mon-Sun 11:30AM-10PM',
+    setup: 'Heated. Enclosed space outdoor.',
+    covid: 'Contactless menu, temperature checks, limited capacity',
+    money: '2',
+    od_img1: 'https://s3-media0.fl.yelpcdn.com/bphoto/MAHyQ1WCTMuqRBdiVv3I5g/o.jpg',
+    od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/75pv3p2DNFfmq9kQL2LajA/o.jpg',
+    fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/_RwdvA-hI5_aeO0hkkNW3g/o.jpg'
+)
+
+Restaurant.create(
+    name: 'Cafe Du Soleil',
+    cuisine: 'French, Wine Bar',
+    address: '2723 Broadway New York, NY 10025',
+    website: 'cafedusoleilny.com',
+    phone: '212-316-5000',
+    menu: 'http://www.cafedusoleilny.com/menus/',
+    hours: 'Mon-Thu 4PM-11PM, Fri-Sun 11AM-11PM',
+    setup: 'Heated. Personal dining pods',
+    covid: 'Contactless payments, limited capacity, social distance regulation',
+    money: 2,
+    od_img1: 'http://www.cafedusoleilny.com/wp-content/uploads/2020/09/BUBBLES2.jpg',
+    od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/sciHRBABfRSVl_xh734UKQ/o.jpg',
+    fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/zt07t1PYsOqOPBxsQCfk2w/o.jpg',
+)
+
+Restaurant.create(
+    name: 'Marcus at Nohu Rooftop',
+    cuisine: 'American',
+    address: '550 Ave At Port Imperial Weehawken, NJ 07086',
+    website: 'https://envuehotel.com/dining/nohu/',
+    phone: '201-683-2132',
+    menu: 'http://envuehotel.com/wp-content/uploads/2021/02/AWE-Marcus-@-Nohu-Rooftop-Menu-2021.pdf',
+    hours: 'Mon-Fri 5PM-12AM, Sat-Sun 11AM-12AM',
+    setup: 'Heated. Personal dining igloo',
+    covid: 'Surfaces sanitize between seatings, extra space between tables, limited capacity, sealed utensils',
+    money: 3,
+    od_img1: 'https://images.otstatic.com/prod1/34927736/4/large.jpg',
+    od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/Z3cRV7CFcbYA_nyViOytpQ/o.jpg',
+    fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/CXBGZTWuUWv6b4w2OMRI4A/o.jpg'
+)
+
+Restaurant.create(
+    name: 'Olio e Piu',
+    cuisine: 'Italian',
+    address: '3 Greenwich Ave New York, NY 10014',
+    website: 'https://olioepiu.nyc/',
+    phone: '212-243-6546',
+    menu: 'https://olioepiu.nyc/menu/',
+    hours: 'Mon-Sun 11AM-11PM',
+    setup: 'Heated. Dividers between tables, Covered outdoor seating',
+    covid: 'Temperature checks, hand sanitizer, contact-slip required, contactless payments and menu',
+    money: 2,
+    od_img1: 'https://images.otstatic.com/prod1/31143887/1/large.jpg',
+    od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/1pSRVg9pgEFtXQAwO1AF-g/o.jpg',
+    fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/zfMdQW5StR08SPg96JWy8g/o.jpg',
+)
+
+Restaurant.create(
+    name: 'Haven Rooftop',
+    cuisine: 'American',
+    address: '132 W 47th St New York, NY 10036',
+    website: 'havenrooftop.com',
+    phone: '212-466-9000',
+    menu: 'http://www.havenrooftop.com/menus/dinner/',
+    hours: 'Mon-Sun 12PM-10PM',
+    setup: 'Rooftop, covered, heated, dividers between tables',
+    covid: 'Masks required, tables distanced, sealed utensils',
+    money: '2',
+    od_img1: 'https://s3-media0.fl.yelpcdn.com/bphoto/tBOVAXx3lNKMf4U5oz5IHQ/o.jpg',
+    od_img2: 'https://s3-media0.fl.yelpcdn.com/bphoto/tFLFQQtBzynTUcwWICfHpA/o.jpg',
+    fd_img: 'https://s3-media0.fl.yelpcdn.com/bphoto/Yz4RuPunTxKnaoQvyR7fCg/o.jpg'
+)
+#----- 10
+
+# Restaurant.create(
+#     name:
+#     cuisine:
+#     address:
+#     website:
+#     phone:
+#     menu:
+#     hours:
+#     setup:
+#     covid:
+#     money: 
+#     od_img1:
+#     od_img2:
+#     fd_img:
+# )
+# Restaurant.create(
+#     name:
+#     cuisine:
+#     address:
+#     website:
+#     phone:
+#     menu:
+#     hours:
+#     setup:
+#     covid:
+#     money: 
+#     od_img1:
+#     od_img2:
+#     fd_img:
+# )
+# Restaurant.create(
+#     name:
+#     cuisine:
+#     address:
+#     website:
+#     phone:
+#     menu:
+#     hours:
+#     setup:
+#     covid:
+#     money: 
+#     od_img1:
+#     od_img2:
+#     fd_img:
+# )
+# Restaurant.create(
+#     name:
+#     cuisine:
+#     address:
+#     website:
+#     phone:
+#     menu:
+#     hours:
+#     setup:
+#     covid:
+#     money: 
+#     od_img1:
+#     od_img2:
+#     fd_img:
+# )
+
+# Restaurant.create(
+#     name:
+#     cuisine:
+#     address:
+#     website:
+#     phone:
+#     menu:
+#     hours:
+#     setup:
+#     covid:
+#     money: 
+#     od_img1:
+#     od_img2:
+#     fd_img:
+# )
+
+#----- 15 
 
 # Restaurant.create(
 #     name:
@@ -217,156 +386,9 @@ Restaurant.create(
 #     od_img2:
 #     fd_img:
 # )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
-# Restaurant.create(
-#     name:
-#     cuisine:
-#     address:
-#     website:
-#     phone:
-#     menu:
-#     hours:
-#     setup:
-#     covid:
-#     money: 
-#     od_img1:
-#     od_img2:
-#     fd_img:
-# )
+
+#----- 20 
+
 # Restaurant.create(
 #     name:
 #     cuisine:
