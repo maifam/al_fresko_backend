604496f8619c47a8856f6d865b3ad238049108f9
Mai Pham <maitphm@gmail.com>
2021-02-26 12:04:59 -0500

    bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,14 @@ class BookmarksController < ApplicationController
         @bookmarks = Bookmark.all
         render json: @bookmarks
     end 
+
+    def create
+        @bookmark = Bookmark.create(bookmark_params)
+        render json: @bookmark
+    end 
+
+    private
+    def bookmark_params
+        params.require(:bookmark).permit(:user_id, :restaurant_id)
+    end
 end
