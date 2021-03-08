19a2780e4a2fd5240ec3f2fa34688eac11f73051
Mai Pham <maitphm@gmail.com>
2021-03-07 21:05:51 -0500

    active storage

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.routes.url_helpers
+
   attributes :id, :username, :name, :image
   has_many :reviews 
   has_many :bookmarks
+
+  def image_url
+    polymorphic_url(object.user_image) if object.user_image.attached?
+  end
+  
 end
