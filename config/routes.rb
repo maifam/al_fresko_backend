b5ba61791530db89eca9cf031af0bb0a0cc198e0
Mai Pham <maitphm@gmail.com>
2021-05-17 23:10:49 -0400

    cleaned up code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  patch "/users/:id", to: "users#update"
+  patch '/users/:id', to: 'users#update'
   post '/login', to: 'users#login'
   get '/profile', to: 'users#show'
   patch '/profile', to: 'users#update'
